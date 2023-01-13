3351b1a0c69eddc960c7086cd609253032bf4e78
Cody Hoang <Cody Hoang@ANDROID45>
2023-01-13 12:17:24 -0500

    Add project files.

---
+++ deliverable1_helloworld/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace deliverable1_helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            string stg = "Hello, World!";
+            Console.WriteLine("Solution (2): "+ stg);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + userName);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter the user's age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The usre's age is: " + age);
+
+
+        }
+    }
+}
